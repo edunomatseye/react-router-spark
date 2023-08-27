4d098c622ca7417e153bcadaf0b544413dda4178
Edun Omatseye <edunomatseye@yahoo.com>
2023-08-27 22:39:22 +0100

    chore: added router routes config file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+import Root from './routes/root'
+import ErrorPage from './error-page'
+import Contact from './routes/contact'
+
+const router = createBrowserRouter([
+  { path: '/', element: <Root />, errorElement: <ErrorPage />},
+  { path: '/contacts:contactId', element: <Contact />}
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
