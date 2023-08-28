bee0658b2a1a5b2fe355d9239ccfd6a7724f1aa7
Edun Omatseye <edunomatseye@yahoo.com>
2023-08-28 12:27:53 +0100

    chore: Added Contact Route to use Loaders

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,23 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
-import Root from './routes/root'
+import Root, { loader as rootLoader } from './routes/root'
 import ErrorPage from './error-page'
 import Contact from './routes/contact'
 
 const router = createBrowserRouter([
-  { path: '/', element: <Root />, errorElement: <ErrorPage />},
-  { path: '/contacts:contactId', element: <Contact />}
+  { 
+    path: '/', 
+    element: <Root />, 
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      {
+        path: 'contacts/:contactId',
+        element: <Contact />,
+      }
+    ]
+  }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,8 +1,14 @@
+import { Outlet, Link, useLoaderData } from "react-router-dom";
+import { getContacts } from "../contact";
+
 export default function Root() {
+    const { contacts } = useLoaderData()
+
     return (
       <>
         <div id="sidebar">
           <h1>React Router Contacts</h1>
+
           <div>
             <form id="search-form" role="search">
               <input
@@ -27,17 +33,36 @@ export default function Root() {
             </form>
           </div>
           <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
+            {contacts.length ? (
+                <ul>
+                {contacts.map((contact) => (
+                    <li key={contact.id}>
+                    <Link to={`contacts/${contact.id}`}>
+                        {contact.first || contact.last ? (
+                        <>
+                            {contact.first} {contact.last}
+                        </>
+                        ) : (
+                        <i>No Name</i>
+                        )}{" "}
+                        {contact.favorite && <span>★</span>}
+                    </Link>
+                    </li>
+                ))}
+                </ul>
+            ) : (
+                <p>
+                    <i>No contacts</i>
+                </p>
+            )}
           </nav>
         </div>
-        <div id="detail"></div>
+        <div id="detail"><Outlet /></div>
       </>
     );
+  }
+
+  export async function loader() {
+    const contacts = await getContacts();
+    return { contacts };
   }
